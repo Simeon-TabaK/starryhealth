3134b3d96ed332b85576ab125099406b064e2851
Simeon-TabaK <stabasimwa@hotmail.com>
2024-04-01 23:13:43 +0200

    landing v1.0.2

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SeoHead from "../components/SeoHead";
 export default function Home() {
   return (
     <>
-      <SeoHead title="Starry Box" />
+      <SeoHead title="Starry Boxx" />
       <Layout>
         <Hero />
         <Feature />
