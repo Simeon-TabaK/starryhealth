afcab033be4c5a1c6ecdb711528d5fc1610a7239
Simeon-TabaK <stabasimwa@hotmail.com>
2024-05-15 18:12:59 +0200

    2layout _app tsx, preset-react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 import "@/styles/embla.css";
 import Layout from "@/components/Layout";
 import SeoHead from "@/components/SeoHead";
-import type { AppProps } from "next/app";
+import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
