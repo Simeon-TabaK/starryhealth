b5a6b4c23c9e939a31da3d09aa34e3ed53213412
Simeon-TabaK <stabasimwa@hotmail.com>
2024-05-15 18:18:54 +0200

    4layout _app tsx, preset-react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,6 @@
-import "@/styles/globals.css";
-import "@/styles/embla.css";
-import Layout from "@/components/Layout";
-import SeoHead from "@/components/SeoHead";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <SeoHead title="SuperLife" />
-      <Layout children={<Component {...pageProps} />}></Layout>
-    </>
-  );
+  return <Component {...pageProps} />
 }
