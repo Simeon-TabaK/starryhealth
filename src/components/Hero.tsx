39801bbc54279dd6c6f06d6f4f7b37a350ae840b
Simeon-TabaK <stabasimwa@hotmail.com>
2024-05-15 16:03:44 +0200

    testmoni

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -38,7 +38,7 @@ const Hero = ({
 
   return (
     <div className="mt-20 ">
-      <ScrollAnimationWrapper className="" >
+      <ScrollAnimationWrapper className="slider" >
         <motion.div
           className="gap-8 py-2 sm:py-12"
           // variants={scrollAnimation}
