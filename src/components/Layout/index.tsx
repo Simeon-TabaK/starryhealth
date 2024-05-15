95704dfe0345b96b0ea69c6193ee8b88059f5c89
Simeon-TabaK <stabasimwa@hotmail.com>
2024-05-15 17:47:31 +0200

    layout tsx

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
-function Layout({ Children }) {
+function Layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <>
       <Header />
-      {Children}
+      {children}
       <Footer />
     </>
   );
