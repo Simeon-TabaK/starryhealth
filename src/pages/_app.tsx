40e00af0227092f13335e79c4d8c5df5856c3f1d
Simeon-TabaK <stabasimwa@hotmail.com>
2024-05-15 17:49:57 +0200

    layout _app  tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <SeoHead title="SuperLife" />
-      <Layout Children={<Component {...pageProps} />}></Layout>
+      <Layout children={<Component {...pageProps} />}></Layout>
     </>
   );
 }
