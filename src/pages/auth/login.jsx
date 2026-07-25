9f0188165a366b3ddb1a2c6bb9168258fa76446f
Simeon-TabaK <stabasimwa@hotmail.com>
2026-07-25 09:36:36 +0200

    fix: resolve invalid page exports in admin and auth directories

---
+++ src/pages/auth/login.jsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Connexion</h1>
+      {/* Votre composant/formulaire de connexion */}
+    </div>
+  );
+}
---
+++ src/pages/auth/register.jsx
@@ -0,0 +1,8 @@
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1>Inscription</h1>
+      {/* Votre composant/formulaire d'inscription */}
+    </div>
+  );
+}
