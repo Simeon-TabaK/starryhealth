3a57662b5a60defd5f6acaefb1abda448b07959b
Simeon-TabaK <stabasimwa@hotmail.com>
2026-09-24 17:01:12 +0200

    fix : milleware to proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname, searchParams } = request.nextUrl;
 
   // 1. Récupération du Host depuis les headers envoyés par Traefik / Dokploy / Vercel
